8617c97097669fe6227c54327933efa6fcade7c3
Carlos Montemuiño <cmontemuino@users.noreply.github.com>
2021-09-07 15:52:26 +0200

    feature(hacks): add function to ask sudo
    Signed-off-by: Carlos Montemuiño <cmontemuino@users.noreply.github.com>

--- hacks/functions.sh
+++ hacks/functions.sh
@@ -8,23 +8,23 @@ set -e
 symlink() {
 	local SRC=$1
 	local DST=$2
-    local OVERWRITTEN=""
+	local OVERWRITTEN=""
 
-    if ! [[ -e "$SRC" ]]; then
-    	substep_error "$SRC does not exist!"
-    	return 1
-    fi
-    if [[ -e "$DST" ]] || [[ -h "$DST" ]]; then
-    	local CURRENT_SRC="$(readlink $DST)"
-    	if [[ "$CURRENT_SRC" == "$SRC" ]]; then
-    		substep_info "$DST symlinked to $SRC already."
-    		return 0
-    	fi
+	if ! [[ -e "$SRC" ]]; then
+		substep_error "$SRC does not exist!"
+		return 1
+	fi
+	if [[ -e "$DST" ]] || [[ -h "$DST" ]]; then
+		local CURRENT_SRC="$(readlink $DST)"
+		if [[ "$CURRENT_SRC" == "$SRC" ]]; then
+			substep_info "$DST symlinked to $SRC already."
+			return 0
+		fi
    
-	    OVERWRITTEN="(Overwritten)"
-        if ! rm -rf "$DST"; then
-       	    substep_error "Failed to remove existing file(s) at $DST."
-       	fi
+		OVERWRITTEN="(Overwritten)"
+		if ! rm -rf "$DST"; then
+			substep_error "Failed to remove existing file(s) at $DST."
+		fi
 	fi
 	if ln -s "$SRC" "$DST"; then
 		substep_success "Symlinked $DST to $SRC. $OVERWRITTEN"
@@ -93,3 +93,15 @@ substep_error() {
 ask_user() {
 	coloredEcho "$1" magenta "---------> "
 }
+
+ask_sudo() {
+        info "Asking for the administrator password upfront..."
+        if sudo -v; then
+                # Keep-alive: update existing `sudo` time stamp until `setup.sh` has finished
+                while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+                success "Sudo credentials updated."
+        else
+                error "Failed to get sudo credentials."
+        fi
+}
+
